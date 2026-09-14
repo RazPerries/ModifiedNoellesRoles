fac23c4b3e73374ed1408d652b9006b1eb1b63d7
RazBerries <147195077+RazPerries@users.noreply.github.com>
2026-09-14 01:21:13 -0500

    Allowed Jester and Conspirator to become Graverobber

--- src/main/java/org/agmas/noellesroles/Noellesroles.java
+++ src/main/java/org/agmas/noellesroles/Noellesroles.java
@@ -130,7 +130,7 @@ public class Noellesroles implements ModInitializer {
     public static Modifier FEATHER = HMLModifiers.registerModifier(new Modifier(FEATHER_ID, new Color(255, 236, 161, 255).getRGB(),null,null,false,false));
     public static Modifier GUESSER = HMLModifiers.registerModifier(new Modifier(GUESSER_ID, new Color(158, 43, 25, 255).getRGB(),new ArrayList<>(List.of(THE_INSANE_DAMNED_PARANOID_KILLER_OF_DOOM_DEATH_DESTRUCTION_AND_WAFFLES)),null,true,false));
     public static Modifier CELEBRITY = HMLModifiers.registerModifier(new Modifier(CELEBRITY_ID, new Color(174, 4, 109, 255).getRGB(), null, null, true, false));
-    public static Modifier GRAVEROBBER = HMLModifiers.registerModifier(new Modifier(GRAVEROBBER_ID, new Color(174, 95, 95, 255).getRGB(),null,new ArrayList<>(List.of(VULTURE, EXECUTIONER)),false,true));
+    public static Modifier GRAVEROBBER = HMLModifiers.registerModifier(new Modifier(GRAVEROBBER_ID, new Color(174, 95, 95, 255).getRGB(),null,new ArrayList<>(List.of(VULTURE, EXECUTIONER, CONSPIRATOR, JESTER)),false,true));
     public static Modifier SIXTH_SENSE = HMLModifiers.registerModifier(new Modifier(SIXTH_SENSE_ID, new Color(244, 201, 152, 255).getRGB(), new ArrayList<>(List.of(WatheRoles.VIGILANTE, JESTER, VULTURE, EXECUTIONER, BARTENDER, CONSPIRATOR)), null, false, true));
     public static Modifier IRON_WILLED = HMLModifiers.registerModifier(new Modifier(IRON_WILLED_ID, new Color(197, 197, 197).getRGB(), new ArrayList<>(List.of(JESTER, VULTURE, EXECUTIONER, CONSPIRATOR)), null, false, true));
     public static Modifier BELLRINGER = HMLModifiers.registerModifier(new Modifier(BELLRINGER_ID, new Color(250, 220, 126).getRGB(), new ArrayList<>(List.of(JESTER, VULTURE, EXECUTIONER, CONSPIRATOR)), null, false, true));
@@ -362,6 +362,12 @@ public class Noellesroles implements ModInitializer {
             }
             if (role.equals(CONSPIRATOR)) {
                 player.giveItemStack(WatheItems.LOCKPICK.getDefaultStack());
+            }
+            if (role.equals(BARTENDER)) {
+                ArrayList<Item> BartenderItems = new ArrayList<>(List.of(WatheItems.OLD_FASHIONED, WatheItems.MARTINI, WatheItems.MOJITO, WatheItems.CHAMPAGNE, WatheItems.COSMOPOLITAN));
+                Collections.shuffle((BartenderItems));
+                player.giveItemStack(BartenderItems.getFirst().getDefaultStack());
+                player.giveItemStack(BartenderItems.getLast().getDefaultStack());
             }
         });
         ServerTickEvents.END_SERVER_TICK.register(((server) -> {
